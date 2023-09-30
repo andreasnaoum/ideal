092d0cd1861ac10f8e68b8354d426cba6b7033c2
Andreas Naoum <a.naoum@mallocprivacy.com>
2023-09-30 23:06:03 +0200

    fetch products + fetch a specific product in json

---
+++ StoreManagement/Manager.py
@@ -0,0 +1,15 @@
+"""
+ * Author:    Andreas Naoum
+ * Created:   30.09.2023
+ *
+ * (c) Copyright iDeal
+"""
+
+
+class ShopifyManager:
+    ADMIN_API_KEY = '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'
+    API_KEY = 'e5bcdda95a0354ed2d4e351585bbf274'
+    API_SECRET = '7166687afd9d38dd9dbb8311eb36cd08'
+
+    def get_api_url(self):
+        return 'https://' + self.API_KEY + ":" + self.ADMIN_API_KEY + '@ideal-2676.myshopify.com/admin/api/2022-07/'
---
+++ StoreManagement/ProductsManagement.py
@@ -0,0 +1,62 @@
+"""
+ * Author:    Andreas Naoum
+ * Created:   30.09.2023
+ *
+ * (c) Copyright iDeal
+"""
+
+import requests
+from Manager import ShopifyManager
+
+
+class ManagerAPI:
+
+    def __init__(self):
+        self.api = URLs()
+
+    class URLs:
+        manager = ShopifyManager()
+
+        # Endpoints
+        products = "products.json"
+        products_count = "products/count.json"
+
+        def get_products_url(self):
+            return self.manager.get_api_url() + products
+
+        def get_product_url(self, product_id):
+            return self.manager.get_api_url() + "products/" + product_id + ".json"
+
+        def new_product_url(self):
+            return self.manager.get_api_url() + products
+
+        def update_product_url(self):
+            return self.manager.get_api_url() + products
+
+        def delete_product_url(self):
+            return self.manager.get_api_url() + products
+
+    def get_products_list(self):
+        return
+
+    def get_product(self, product_id):
+        request = self.api.get_product_url(product_id)
+        fetch_product = requests.get(request)
+        return fetch_product.json()
+
+    def create_product(self, product):
+        return
+
+    def update_product(self, product):
+        return
+
+    def set_inactive_product(self, product_id):
+        return
+
+    def delete_product(self, product):
+        return
+
+
+if __name__ == '__main__':
+    manager = ManagerAPI()
+    print(manager.get_product('8510061379918'))
